a57471bfd6422ff901c9a29f0a2547ec51fbcd26
mihaelko27 <miha.brunec1@gmail.com>
2025-05-12 20:09:12 +0200

    SCRUM-70 implementiral vse view-e za prikaz podatkov iz baze

---
+++ desktop/src/main/kotlin/viewTables/ViewLocations.kt
@@ -0,0 +1,86 @@
+package viewTables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+// za teste dokler ni povezave na bazo
+internal data class Location(
+    val longitude: Double,
+    val latitude: Double
+)
+
+internal val locations = listOf(
+    Location(longitude = 14.505751, latitude = 46.056947),
+    Location(longitude = 15.645881, latitude = 45.815399),
+    Location(longitude = 13.593354, latitude = 45.546299)
+)
+
+
+@Composable
+internal fun LocationCard(location: Location) {
+    Surface(
+        modifier = Modifier
+            .padding(24.dp)
+            .width(400.dp)
+            .height(220.dp)
+            .background(Color.White, shape = RoundedCornerShape(12.dp)),
+        shape = RoundedCornerShape(8.dp),
+        color = Color(0xFFFFFFFF),
+
+        ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text("Longitude: ${location.longitude}")
+            Spacer(modifier = Modifier.height(4.dp))
+            Text("Latitude: ${location.latitude}")
+            Spacer(modifier = Modifier.height(4.dp))
+
+        }
+    }
+}
+
+
+@Composable
+fun ViewLocation() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 300.dp)
+            .background(Color(0xFFE3F2FD)),
+        contentAlignment = Alignment.Center
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 550.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentPadding = PaddingValues(8.dp)
+        ) {
+            items(locations) { location ->
+                LocationCard(location)
+            }
+        }
+
+    }
+
+}
