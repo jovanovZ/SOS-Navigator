6e0753e8c89a34f97999eabefa6db26207e43ce3
jovanovZ <jovanovzdravko3011@gmail.com>
2025-05-20 18:29:40 +0200

    SCRUM-32 Implementacija Simulacija kontrolerja

---
+++ backend/routes/simulationRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const simulationController = require('../controllers/simulationController');
+
+router.get('/all', simulationController.getAllSimulations);
+router.get('/:simulationId', simulationController.getSimulationById);
+router.get('/user/:userId', simulationController.getSimulationsByUserId);
+router.get('/accident/:accidentId', simulationController.getSimulationsByAccidentId);
+router.get('/type/:typeOfServices', simulationController.getSimulationsByServiceType);
+
+router.post('/create', simulationController.createSimulation);
+router.put('/update/:simulationId', simulationController.updateSimulation);
+
+router.delete('/delete/:simulationId', simulationController.deleteSimulation);
+
+module.exports = router;
